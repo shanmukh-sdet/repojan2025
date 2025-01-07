79465fc44bc2499255be9bcc690b27cf57b2906d
ssrinivas123 <shanmukha.srinivas@glooko.com>
2025-01-07 22:56:04 +0530

    added code Heloo world and someother in new branch repo2025

---
+++ src/ex_05_jan_2025/helloGIT.java
@@ -0,0 +1,11 @@
+package ex_05_jan_2025;
+
+public class helloGIT {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("new line for GIT");
+        System.out.println("craeted new profile");
+    }
+}
+
+
