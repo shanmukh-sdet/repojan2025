d348a15a13f310bbee0a75300adc8132214429a4
ssrinivas123 <shanmukha.srinivas@glooko.com>
2025-01-07 23:56:18 +0530

    first commit

--- src/ex_05_jan_2025/helloclass.java
+++ src/ex_05_jan_2025/helloclass.java
@@ -3,5 +3,6 @@ package ex_05_jan_2025;
 public class helloclass {
     public static void main(String[] args) {
         System.out.println("created new profile with new java class");
+        System.out.println("created new profile with new java class1");
     }
 }
