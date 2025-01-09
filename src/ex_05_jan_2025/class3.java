dfaca6f72d12daacb4ff33e096bdd171f55b7807
ssrinivas123 <shanmukha.srinivas@glooko.com>
2025-01-09 20:54:00 +0530

    second commit

--- src/ex_05_jan_2025/class3.java
+++ src/ex_05_jan_2025/class3.java
@@ -1,4 +1,8 @@
 package ex_05_jan_2025;
 
 public class class3 {
+    public static void main(String[] args) {
+        System.out.println("my second git commit code");
+        System.out.println("my second git commit code java");
+    }
 }
