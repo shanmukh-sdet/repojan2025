746ece7191854dd5455302a6cbc6769be7cc3b4c
ssrinivas123 <shanmukha.srinivas@glooko.com>
2025-01-09 20:50:14 +0530

    second commit

---
+++ src/ex_05_jan_2025/class3.java
@@ -0,0 +1,4 @@
+package ex_05_jan_2025;
+
+public class class3 {
+}
--- src/ex_05_jan_2025/helloclass.java
+++ src/ex_05_jan_2025/helloclass.java
@@ -4,5 +4,7 @@ public class helloclass {
     public static void main(String[] args) {
         System.out.println("created new profile with new java class");
         System.out.println("created new profile with new java class1");
+        System.out.println("created new profile with new java class2git");
+
     }
 }
