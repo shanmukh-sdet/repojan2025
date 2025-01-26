39f64ab9de326e483dd9203b6a75588aa6eaad7c
ssrinivas123 <shanmukha.srinivas@glooko.com>
2025-01-26 20:57:02 +0530

    first commit

---
+++ src/ex_05_jan_2025/Table9_Print.java
@@ -0,0 +1,9 @@
+package ex_05_jan_2025;
+
+public class Table9_Print {
+    public static void main(String[] args) {
+        int a  = 9;
+        for (int i =1;i<=10;i++)
+        System.out.println("9*"+i+"="+(9*i));
+    }
+}
